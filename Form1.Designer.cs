c76cb4eb33b90d63a083d0cb9e0a945724fd37e9
Hydria <liverpol2222@gmail.com>
2015-07-15 08:48:28 +0100

    full .cs files
    should open in most visual studio programs, just overlooking for
    cautious people

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace osutosmconverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.input_btn = new System.Windows.Forms.Button();
+            this.convert_btn = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(505, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // input_btn
+            // 
+            this.input_btn.Location = new System.Drawing.Point(523, 20);
+            this.input_btn.Name = "input_btn";
+            this.input_btn.Size = new System.Drawing.Size(75, 23);
+            this.input_btn.TabIndex = 1;
+            this.input_btn.Text = "Input";
+            this.input_btn.UseVisualStyleBackColor = true;
+            this.input_btn.Click += new System.EventHandler(this.input_btn_Click);
+            // 
+            // convert_btn
+            // 
+            this.convert_btn.Location = new System.Drawing.Point(523, 75);
+            this.convert_btn.Name = "convert_btn";
+            this.convert_btn.Size = new System.Drawing.Size(75, 23);
+            this.convert_btn.TabIndex = 4;
+            this.convert_btn.Text = "Go";
+            this.convert_btn.UseVisualStyleBackColor = true;
+            this.convert_btn.Click += new System.EventHandler(this.convert_btn_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(322, 15);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Speed change in % (For Example, for 1.5x, enter \"50\")";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(347, 76);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(170, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(123, 49);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(394, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.textBox5.Location = new System.Drawing.Point(12, 52);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(105, 15);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "Audio Filename:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.textBox6.Location = new System.Drawing.Point(526, 52);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(100, 15);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Text = ".mp3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 106);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.convert_btn);
+            this.Controls.Add(this.input_btn);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Hydria\'s Osu!Mania Speed Changer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button input_btn;
+        private System.Windows.Forms.Button convert_btn;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace osutosmconverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void input_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Select the .osu file";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "Osu files (*.osu)|*.osu|Osu files (*.osu)|*.osu";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fdlg.FileName;
+            }
+        }
+
+        private void convert_btn_Click(object sender, EventArgs e)
+        {
+            //imports all lines of the text into a string array
+            string[] lines = System.IO.File.ReadAllLines(textBox1.Text);
+
+            //error checking
+            string fullmode = lines[9];
+            string mode = fullmode.Substring(6);
+            if (mode != "3")
+            {
+                MessageBox.Show("This is not an osu!mania file. Please choose an osu!mania file.");
+            }
+            else
+            {
+                //changes the BPM value entered into one I can use
+                decimal speedchange = Convert.ToDecimal(textBox3.Text);
+                decimal totalspeed = 100 + speedchange;
+                decimal totalspeedchange = (totalspeed) / 100;
+                //audio filename setting
+                lines[3] = "AudioFilename: " + textBox4.Text + ".mp3";
+                // ----- LOCATE DIFFICULTY ----- //
+                int j = 1;
+                int difficulty = 0;
+                string creator = "Creator:";
+                foreach (string x in lines)
+                {
+                    if (x.Contains(creator))
+                    {
+                        difficulty = j;
+                    }
+                    else
+                    {
+                        j++;
+                    }
+                }
+                // ----- LOCATE TIMING POINTS ----- //
+                int i = 1;
+                int offset = 0;
+                string timingpoints = "[TimingPoints]";
+                foreach (string x in lines)
+                {
+                    if (x.Contains(timingpoints))
+                    {
+                        offset = i;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+                // ----- LOCATE HIT OBJECTS ----- //
+                int m = -2;
+                int fulltime = 0;
+                string hitobjects = "[HitObjects]";
+                foreach (string x in lines)
+                {
+                    if (x.Contains(hitobjects))
+                    {
+                        fulltime = m;
+                    }
+                    else
+                    {
+                        m++;
+                    }
+                }
+
+                // ----- TEXT BEFORE DIFFICULTY NAME ----- //
+                string firstbigbitoftext = null;
+                string stringdiff = lines[difficulty];
+                for (int k = 2; k < difficulty; k++)
+                {
+                    firstbigbitoftext = firstbigbitoftext + lines[k] + Environment.NewLine;
+                }
+
+                // ----- TEXT BEFORE TIMING POINTS ----- //
+                string secondbigbitoftext = null;
+                for (int l = (difficulty + 1); l < offset; l++)
+                {
+                    secondbigbitoftext = secondbigbitoftext + lines[l] + Environment.NewLine;
+                }
+
+                // ---- TEXT BEFORE HIT OBJECTS ----- //
+                string thirdbigbitoftext = null;
+                for (int n = offset; n < fulltime; n++)
+                {
+                    //changes the first value (offset) to the desired rate and adds the value to the string
+                    string[] linearray = lines[n].Split(new char[] { ',' });
+                    string firstvalue = linearray[0];
+                    decimal decfirstvalue = Convert.ToDecimal(firstvalue);
+                    decfirstvalue = decfirstvalue / totalspeedchange;
+                    decfirstvalue = Math.Floor(decfirstvalue);
+                    thirdbigbitoftext = thirdbigbitoftext + decfirstvalue + ",";
+
+                    //checks the second value to see if it's a BPM change or an SV change and if 
+                    //it's a BPM change, works out the new BPM and adds the value to the string.
+                    string secondvalue = linearray[1];
+                    decimal decsecondvalue = Convert.ToDecimal(secondvalue);
+                    if (decsecondvalue > 0)
+                    {
+                        decimal resetvalue = 60000 / decsecondvalue;
+                        decimal newvalue = resetvalue * totalspeedchange;
+                        decsecondvalue = 60000 / newvalue;
+                    }
+                    thirdbigbitoftext = thirdbigbitoftext + decsecondvalue + ",";
+
+                    //does a for loop for the rest of the objects in the line
+                    for (int o = 2; o < linearray.Length; o++)
+                    {
+                        thirdbigbitoftext = thirdbigbitoftext + linearray[o] + ",";
+                    }
+                    //removes the last comma and adds a new line
+                    thirdbigbitoftext = thirdbigbitoftext.Truncate(thirdbigbitoftext.Length - 1);
+                    thirdbigbitoftext = thirdbigbitoftext + Environment.NewLine;
+
+                }
+
+                // ----- REST OF TEXT ----- //
+                string fourthbigbitoftext = null;
+                for (int p = fulltime + 3; p < (lines.Length-1); p++)
+                {
+                    string[] linearray = lines[p].Split(new char[] { ',' });
+                    fourthbigbitoftext = fourthbigbitoftext + linearray[0] + "," + linearray[1] + ",";
+                    string thirdvalue = linearray[2];
+                    decimal decthirdvalue = Convert.ToDecimal(thirdvalue);
+                    decthirdvalue = decthirdvalue / totalspeedchange;
+                    decthirdvalue = Math.Floor(decthirdvalue);
+                    fourthbigbitoftext = fourthbigbitoftext + decthirdvalue + ",";
+                    //need to check if it's an LN now since LNs need a different edit to normal notes.
+                    string linecheck = linearray[3];
+                    if (linecheck == "128")
+                    {
+                        fourthbigbitoftext = fourthbigbitoftext + linearray[3] + "," + linearray[4] + ",";
+                        string linefind = linearray[5];
+                        string[] secondlinearray = linefind.Split(new char[] { ':' });
+                        string fifthvalue = secondlinearray[0];
+                        decimal decfifthvalue = Convert.ToDecimal(fifthvalue);
+                        decfifthvalue = decfifthvalue / totalspeedchange;
+                        decfifthvalue = Math.Floor(decfifthvalue);
+                        fourthbigbitoftext = fourthbigbitoftext + decfifthvalue + ":0:0:0:0:";
+                    }
+                    else
+                    {
+                        for (int q = 3; q < linearray.Length; q++)
+                        {
+                            fourthbigbitoftext = fourthbigbitoftext + linearray[q] + ",";
+                        }
+                        fourthbigbitoftext = fourthbigbitoftext.Truncate(fourthbigbitoftext.Length - 1);
+                    }
+                    fourthbigbitoftext = fourthbigbitoftext + Environment.NewLine;
+                }
+
+                MessageBox.Show(firstbigbitoftext + Environment.NewLine + secondbigbitoftext);
+                string fileoutput = "osu file format v14 " + Environment.NewLine +
+                    "//Conversion completed by Hydria. http://www.hydriaslist.ga/ " + Environment.NewLine + Environment.NewLine +
+                    firstbigbitoftext + stringdiff + " " + totalspeedchange + "x" + Environment.NewLine + secondbigbitoftext +
+                    thirdbigbitoftext + Environment.NewLine + Environment.NewLine + "[HitObjects]" + Environment.NewLine + fourthbigbitoftext;
+
+                int outputfilelength = textBox1.Text.Length;
+                string firstoutputfile = textBox1.Text.Truncate(outputfilelength - 5);
+
+                string outputfile = firstoutputfile + " " + totalspeedchange + "x].osu";
+                File.WriteAllText(outputfile, fileoutput);
+                MessageBox.Show("Conversion Complete.");
+            }
+        }
+    }
+}
+public static class StringExt
+    {
+        public static string Truncate(this string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+        }
+    }
